09e72391c4241533977eeec1deae804c1db4a26f
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-03-07 11:13:47 +0100

    feat: ajout du service et du composant messages dans le composant app
    closes #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Personne} from './personne'
+import {MessagesService} from './services/messages.service';
+import {ShowMessagesComponent} from './show-messages.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ShowMessagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'gestion-depenses';
+
+  constructor(public messagesService: MessagesService) {
+    messagesService.add('Bonjour du composant app')
+  }
 }
