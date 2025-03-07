ca24fd7966f5cfc26ca2f2052fb5ba79dde7b504
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-03-07 10:54:37 +0100

    feat: création du service messages

---
+++ src/app/services/messages.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessagesService {
+  messages: string[] = [];
+
+  add(message: string): void {
+    this.messages.push(message);
+  }
+
+  clear(): void {
+    this.messages = [];
+  }
+
+  isEmpty(): boolean {
+    return this.messages.length === 0;
+  }
+}
