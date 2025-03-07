f1b0ee279960840aac74684929271934565b511f
Carl Kolodziejski <carl.kolodziejski19@gmail.com>
2025-03-07 21:49:58 +0100

    feat: ajout de routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {DashboardComponent} from './dashboard.component';
+import {PageNotFoundComponent} from './page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
