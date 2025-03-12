6e943455ceca7a049c8f502b268fb528e31edad7
quentin_tripognez <quentin_tripognez@ens.univ-artois.fr>
2025-03-12 15:25:29 +0100

    feat: Ajout des boutons de navigation.
    closes #4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {MessagesService} from './services/messages.service';
 import {ShowMessagesComponent} from './show-messages.component';
 import {PersonnesListeComponent} from './personnes-liste.component';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ShowMessagesComponent, PersonnesListeComponent],
+  imports: [RouterOutlet, ShowMessagesComponent, PersonnesListeComponent, MatToolbar, MatIcon, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
