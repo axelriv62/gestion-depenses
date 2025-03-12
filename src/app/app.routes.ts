5d9e5078350f38f29a1204dd0369c2c7f01f15fc
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-03-12 16:56:02 +0100

    feat: ajout du composant et de la route pour afficher le détail des personnes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard.component';
 import {PageNotFoundComponent} from './page-not-found.component';
 import {PersonnesListeComponent} from './personnes-liste.component';
+import {PersonnesDetailsComponent} from './personnes-details.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'personnes', component: PersonnesListeComponent},
+  {path: 'depenses', component: PersonnesDetailsComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
---
+++ src/app/personnes-details.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personnes-details',
+  imports: [],
+  template: `
+    <p>
+      personnes-details works!
+    </p>
+  `,
+  styles: ``
+})
+export class PersonnesDetailsComponent {
+
+}
