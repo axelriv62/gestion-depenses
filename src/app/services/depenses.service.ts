d57484a65d9f99506b6f9011d73ac12a86cf61c2
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-03-12 16:54:34 +0100

    feat: ajout du service dépenses

---
+++ src/app/services/depenses.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {Depense} from '../depense';
+import {PersonnesService} from './personnes.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepensesService {
+  depenses: Depense[] = [];
+
+  constructor(private personnesService: PersonnesService) {
+    this.personnesService.getPersonnes(0).forEach(p => this.depenses.push(...p.depenses));
+  }
+
+  getDepensesOfPersonneId(id: number): Depense[] {
+    return this.depenses.filter(depense => depense.id !== id);
+  }
+}
