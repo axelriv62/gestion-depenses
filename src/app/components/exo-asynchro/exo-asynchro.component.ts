a6af3c7bd7cca686728d742f8ef77d772b1805f8
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-03-17 11:30:26 +0100

    feat: ajout de la fonction asynchrone myFunction et de son appel dans la fonction callMyFunction

--- src/app/components/exo-asynchro/exo-asynchro.component.ts
+++ src/app/components/exo-asynchro/exo-asynchro.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-exo-asynchro',
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './exo-asynchro.component.html',
   styleUrl: './exo-asynchro.component.css'
 })
-export class ExoAsynchroComponent {
+export class ExoAsynchroComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.callMyFunction();
+  }
+
+  async myFunction(): Promise<string> {
+    return "Hello";
+  }
+
+  async callMyFunction() {
+    const result = await this.myFunction();
+    console.log(result);
+  }
 
 }
