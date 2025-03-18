6d58239f33e01bade0618f3ecd62380e9a06c4a1
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-03-18 17:01:44 +0100

    feat: ajout de la simulation de récupération de données avec des tentatives

--- src/app/components/exo-asynchro/exo-asynchro.component.ts
+++ src/app/components/exo-asynchro/exo-asynchro.component.ts
@@ -8,9 +8,13 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ExoAsynchroComponent implements OnInit {
 
+  tabId: number[] = [1, 2, 3, 4, 5];
+
   ngOnInit(): void {
-    this.getData();
+    this.getDataItem();
   }
+
+  // Simulaton de fonction asynchrone (question 2.1)
 
   async myFunction(): Promise<string> {
     return "Hello";
@@ -20,6 +24,9 @@ export class ExoAsynchroComponent implements OnInit {
     const result = await this.myFunction();
     console.log(result);
   }
+
+
+  // Simulation de récupération de données (questions 2.2 et 2.3)
 
   fetchData() {
     setTimeout(() => {
@@ -32,11 +39,47 @@ export class ExoAsynchroComponent implements OnInit {
     }, 2000);
   }
 
-
   async getData() {
     console.log("Récupération des données...");
     const data = await this.fetchData();
     console.log(data);
     console.log("Données récupérées");
+  }
+
+
+  // Simulation de récupération de données avec tentatives (questions 2.4 et 2.5)
+
+  fetchItem(id: number): Promise<string> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const isSuccess = Math.random() > 0.5;
+        if (isSuccess) {
+          resolve(`Données de l'élément ${id}`);
+        } else {
+          reject(`Échec de la récupération des données de l'élément ${id}`);
+        }
+      }, 2000);
+    });
+  }
+
+  async getDataItem() {
+    for (const id of this.tabId) {
+      console.log(`Récupération des données de l'élément ${id}`);
+      let tentative = 0;
+      while (tentative < 3) {
+        console.log(`Tentative ${tentative + 1}`);
+        try {
+          const data = await this.fetchItem(id);
+          console.log(data);
+          break;
+        } catch (error) {
+          console.error(error);
+          tentative++;
+          if (tentative === 3) {
+            console.error(`Impossible de récupérer les données de l'élément ${id} après 3 tentatives.`);
+          }
+        }
+      }
+    }
   }
 }
