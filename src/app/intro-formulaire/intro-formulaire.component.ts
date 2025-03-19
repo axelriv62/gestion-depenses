7223341a83af9df8818ee9fa892f27b47d79d035
quentin_tripognez <quentin_tripognez@ens.univ-artois.fr>
2025-03-19 13:56:32 +0100

    feat: création du champ de saisi du numéro de téléphone
    closes #29 #30

--- src/app/intro-formulaire/intro-formulaire.component.ts
+++ src/app/intro-formulaire/intro-formulaire.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+// Code récupéré sur le sujet
+
+import {Component} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {MatFormField} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-intro-formulaire',
-  imports: [],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput
+  ],
   templateUrl: './intro-formulaire.component.html',
   styleUrl: './intro-formulaire.component.css'
 })
 export class IntroFormulaireComponent {
+  formulaire = new FormGroup({
+    numeroTelCtrl: new FormControl('')
+  });
 
+  constructor() {
+  }
+
+  get numeroTelCtrl(): AbstractControl | null {
+    return this.formulaire.get('numeroTelCtrl');
+  }
 }
