20808e8b298cfda6e141738e1f0979067ddeda1c
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-03-21 23:37:00 +0100

    fix: adaptation du composant personnes-liste avec le service personnes

--- src/app/components/personnes-liste/personnes-liste.component.ts
+++ src/app/components/personnes-liste/personnes-liste.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit, signal, ViewChild} from '@angular/core';
 import {Personne} from '../../personne';
 import {PersonnesService} from '../../services/personnes.service';
 import {MessagesService} from '../../services/messages.service';
@@ -14,7 +14,7 @@ import {
   MatTableModule
 } from '@angular/material/table';
 import {NgClass} from '@angular/common';
-import {Subscription} from 'rxjs';
+import {noop, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-personnes-liste',
@@ -33,20 +33,24 @@ import {Subscription} from 'rxjs';
   templateUrl: './personnes-liste.component.html',
   styleUrl: './personnes-liste.component.css'
 })
-export class PersonnesListeComponent {
+export class PersonnesListeComponent implements OnInit, OnDestroy {
   @ViewChild(MatTable) table!: MatTable<Personne>
-  personnes: Personne[] = [];
+  personnes = signal<Personne[]>([]);
   columns = ['id', 'nom', 'prenom', 'plafond', 'depenses', 'details'];
   horsLimite: boolean = false;
   queryParamSubscription!: Subscription;
+  totalDepensesCache: { [key: number]: number } = {};
+  isOverPlafondCache: { [key: number]: boolean } = {};
 
   constructor(private readonly personnesService: PersonnesService, private readonly messagesService: MessagesService, private readonly route: ActivatedRoute) {
   }
 
+  // comme dans le sujet
   ngOnInit() {
     this.queryParamSubscription = this.route.queryParamMap.subscribe(params => {
+      console.log("appel de ngOnInit dans le composant personnes-liste");
       this.horsLimite = params.get('horsLimite') === 'true';
-      this.getPersonnes(0);
+      this.getPersonnes(0).then(noop)
     });
   }
 
@@ -54,28 +58,58 @@ export class PersonnesListeComponent {
     this.queryParamSubscription.unsubscribe();
   }
 
-  getPersonnes(sort: number) {
-    this.personnes = this.personnesService.getPersonnes(sort);
-    if (this.horsLimite) {
-      this.personnes = this.personnes.filter(personne => this.isOverPlafond(personne));
-    }
-    this.table.renderRows()
-    this.messagesService.clear()
-    if (sort == 0) {
-      this.messagesService.add('Tri par ID');
-    } else if (sort == 1) {
-      this.messagesService.add('Tri par Nom (A-Z)');
-    } else {
-      this.messagesService.add('Tri par Nom (Z-A)');
+  // comme dans le sujet
+  async getPersonnes(sort: number) {
+    console.log("appel de getPersonnes dans le composant personnes-liste");
+
+    try {
+      let personnes = await this.personnesService.getPersonnes(sort);
+
+      if (this.horsLimite) {
+        personnes = personnes.filter(personne => this.isOverPlafondCache[personne.id]);
+        this.table.renderRows()
+      }
+
+      for (const personne of personnes) {
+        this.totalDepensesCache[personne.id] = await this.getTotalDepenses(personne);
+        this.isOverPlafondCache[personne.id] = await this.isOverPlafond(personne);
+      }
+
+      this.personnes.set(personnes);
+
+      this.messagesService.clear()
+      if (sort == 0) {
+        this.messagesService.add('Tri par ID');
+      } else if (sort == 1) {
+        this.messagesService.add('Tri par Nom (A-Z)');
+      } else {
+        this.messagesService.add('Tri par Nom (Z-A)');
+      }
+
+    } catch (error) {
+      this.messagesService.add("Erreur lors du chargement des personnes:" + error);
     }
   }
 
-  getTotalDepenses(personne: Personne): number {
-    return this.personnesService.totalDepenses(personne);
+  async getTotalDepenses(personne: Personne): Promise<number> {
+    console.log("appel de getTotalDepenses dans le composant personnes-liste pour la personne " + personne.id);
+    if (this.totalDepensesCache[personne.id] !== undefined) {
+      return this.totalDepensesCache[personne.id];
+    }
+    const total = await this.personnesService.totalDepenses(personne);
+    this.totalDepensesCache[personne.id] = total;
+    return total;
   }
 
-  isOverPlafond(personne: Personne): boolean {
-    return this.personnesService.totalDepenses(personne) > personne.plafond;
+  async isOverPlafond(personne: Personne): Promise<boolean> {
+    console.log("appel de isOverPlafond dans le composant personnes-liste pour la personne " + personne.id);
+    if (this.isOverPlafondCache[personne.id] !== undefined) {
+      return this.isOverPlafondCache[personne.id];
+    }
+    const totalDepenses = await this.getTotalDepenses(personne);
+    const isOver = totalDepenses > personne.plafond;
+    this.isOverPlafondCache[personne.id] = isOver;
+    return isOver;
   }
 
 }
