56900e8dfbc8ce9b4bc54ac0ff6956782073bcaf
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-03-21 23:37:40 +0100

    fix: adaptation du composant personnes-details avec les service personnes et dépenses

--- src/app/components/personnes-details/personnes-details.component.ts
+++ src/app/components/personnes-details/personnes-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, signal, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {PersonnesService} from '../../services/personnes.service';
 import {DepensesService} from '../../services/depenses.service';
@@ -17,6 +17,7 @@ import {
 } from '@angular/material/table';
 import {Depense} from '../../depense';
 import {MessagesService} from '../../services/messages.service';
+import {Personne} from '../../personne';
 
 @Component({
   selector: 'app-personnes-details',
@@ -40,34 +41,48 @@ export class PersonnesDetailsComponent {
   @ViewChild(MatTable) table!: MatTable<Depense>
   id: number;
   columns = ['id', 'dd', 'nature', 'libelle', 'montant'];
-  depenses: Depense[] = [];
+  depenses = signal<Depense[]>([]);
   natures: string[] = ['Alimentaire', 'Loisirs', 'Voiture', 'Habitat', 'Sport', 'Vacances']
+  personne!: Personne;
 
   constructor(private route: ActivatedRoute, private personnesService: PersonnesService, private depensesService: DepensesService, private messagesService: MessagesService) {
     this.id = +(this.route.snapshot.paramMap.get('id') || 1);
-    this.depenses = depensesService.getDepensesOfPersonneId(this.id);
   }
 
-  getPersonne() {
-    return this.personnesService.getPersonne(this.id);
+  ngOnInit() {
+    console.log("appel de ngOnInit dans le composant personnes-details pour la personne " + this.id);
+    this.getPersonne().then(() => this.getDepensesOfPersonneId(0))
   }
 
-  getDepensesOfPersonneId(sort?: number) {
-    this.depenses = this.depensesService.getDepensesOfPersonneId(this.id, sort);
-    this.messagesService.clear()
-    this.table.renderRows()
-    if (sort == 0) {
-      this.messagesService.add('Tri par date');
-    } else if (sort == 1) {
-      this.messagesService.add('Tri par montant (décroissant)');
-    } else {
-      this.messagesService.add('Tri par nature et montant');
+  async getPersonne() {
+    console.log("appel de getPersonne dans le composant personnes-details pour la personne " + this.id);
+    this.personne = await this.personnesService.getPersonne(this.id)
+  }
+
+  async getDepensesOfPersonneId(sort: number) {
+    console.log("appel de getDepensesOfPersonneId dans le composant personnes-details pour la personne " + this.id);
+
+    try {
+      const depenses = await this.depensesService.getDepensesOfPersonneId(this.id, sort);
+      this.depenses.set(depenses)
+
+      this.messagesService.clear()
+      if (sort == 0) {
+        this.messagesService.add('Tri par date');
+      } else if (sort == 1) {
+        this.messagesService.add('Tri par montant (décroissant)');
+      } else {
+        this.messagesService.add('Tri par nature et montant');
+      }
+
+    } catch (error) {
+      this.messagesService.add("Erreur lors du chargement des dépenses:" + error);
     }
   }
 
-  filtreDepenses(filtre: Event) {
+  async filtreDepenses(filtre: Event) {
     let nature = (filtre.target as HTMLSelectElement).value;
-    this.depenses = this.depensesService.filtreDepenses(this.id, nature);
+    this.depenses.set(await this.depensesService.filtreDepenses(this.id, nature));
     this.messagesService.clear()
     this.table.renderRows()
     if (nature !== '') {
