16e730fc4d274a002db269d976a8eaabdcb95052
Axel Rivière <axel_riviere@ens.univ-artois.fr>
2025-03-24 14:10:58 +0100

    feat: modification du menu en fonction de si l'utilisateur est identifié ou non

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {MessagesService} from './services/messages.service';
 import {ShowMessagesComponent} from './components/show-messages/show-messages.component';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
 import {MatButton} from '@angular/material/button';
+import {AuthService} from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,8 @@ import {MatButton} from '@angular/material/button';
 })
 export class AppComponent {
   title = 'gestion-depenses';
+  auth = inject(AuthService);
+  protected readonly AuthService = AuthService;
 
   constructor(public messagesService: MessagesService) {
     messagesService.add('Bonjour du composant app')
