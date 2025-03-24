e1fd0fd036771afb1db741722b1e1dc5b602badf
Kolodziejski Carl <carl_kolodziejski@ens.univ-artois.fr>
2025-03-24 13:26:07 +0100

    feat: formulaire pour visualiser les différents états d'un champ de saisie

--- src/app/components/form-val-email/form-val-email.component.ts
+++ src/app/components/form-val-email/form-val-email.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormField} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-form-val-email',
-  imports: [],
+  imports: [
+    MatFormField,
+    ReactiveFormsModule,
+    MatInput
+  ],
   templateUrl: './form-val-email.component.html',
   styleUrl: './form-val-email.component.css'
 })
-export class FormValEmailComponent {
 
+
+export class FormValEmailComponent {
+  fb = inject(FormBuilder);
+
+  formulaire = this.fb.group({
+    email: new FormControl('',
+      [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}$')]),
+  });
+
+  get email(): AbstractControl | null {
+    return this.formulaire.get('email');
+  }
 }
+
